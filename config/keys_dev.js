61c134451e8162653c36b1fd7d4c3c02c29d1150
Vivek Yadav <vivekyadav264@gmail.com>
2018-12-11 15:42:58 +0000

    keys_dev.js edited online with Bitbucket

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,8 +1,8 @@
 module.exports = {
-  mongoURI: 'mongodb://1101:1101tech@ds229878.mlab.com:29878/stealthsonics',
+  mongoURI: 'mongodb://@ds229878.mlab.com:29878/stealthsonics',
   secretOrKey: 'secret',
   stripePublishableKey: 'pk_test_nLJp2q960QplJ4lxFMNNCCsT',
-  stripeSecretKey: 'sk_test_gnepLQhbblHIEdWzWr8noToW'
+  stripeSecretKey: ''
 };
 //mongoURI: 'mongodb://localhost/mernApp', 
 
